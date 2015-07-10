e57c12c89ff3565bf681b73d192f0553e4d18506
Luca Fiaschi <luca.fiaschi@rocket-internet.de>
2015-07-10 17:01:52 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_module = Extension(
-    "fastbetabino/_betactr",
-    ["fastbetabino/_betactr.pyx"],
+    "fastbetabino",
+    ["fastbetabino.pyx"],
     extra_compile_args=['-fopenmp'],
     extra_link_args=['-fopenmp'],
 )
